7df7daadb36918cd01842a120a28c26bea79b459
gayathrichelluri <gayathri4040#gmail.com>
2024-08-28 23:52:09 +1000

    04 - pass data as props

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -7,16 +7,24 @@ const Body = () => {
         <div className="body">
             <div className="search-field">Search</div>
             <div className="card-container">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card
+                    name={"Dosa Hut"}
+                    cuisine={"South Indian Breakfast, Indian"}
+                    rating={"4.5 Stars"}
+                    deliveryETA={"25 mins"}
+                />
+                <Card
+                    name={"Ginger"}
+                    cuisine={"North Indian Breads, Indian"}
+                    rating={"4.3 Stars"}
+                    deliveryETA={"15 mins"}
+                />
+                <Card
+                    name={"Chul Ho"}
+                    cuisine={"Fast Food, Chinese"}
+                    rating={"4.2 Stars"}
+                    deliveryETA={"20 mins"}
+                />
             </div>
         </div>
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import logo from '../../resources/images/logo.png'
 import './index.css';
 
-const Card = (props) => {
+const Card = ({ name, cuisine, rating, deliveryETA }) => {
     return (
         <div className='res-card'>
             <img src={logo} className='res-img' />
             <div className='res-details'>
-                <div className='res-name'>Restaurant Name</div>
-                <div className='res-cuisine'>Cuisine</div>
-                <div className='res-rating'>Rating</div>
-                <div className='res-eta'>Delivery ETA</div>
+                <div className='res-name'>{name}</div>
+                <div className='res-cuisine'>{cuisine}</div>
+                <div className='res-rating'>{rating}</div>
+                <div className='res-eta'>{deliveryETA}</div>
             </div>
         </div>
     )
