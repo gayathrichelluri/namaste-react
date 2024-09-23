1ae034f9e826e7d1e9fba46237fb3612162cf278
gayathrichelluri <gayathri4040#gmail.com>
2024-09-23 20:44:43 +1000

    11 - added add button and some refactor

--- src/api/getRestaurantMenu.js
+++ src/api/getRestaurantMenu.js
@@ -1,31 +1,38 @@
-
 export const getRestaurantMenu = async (resId) => {
-    const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=${resId}`);
-    const result = await response.json();
+	const response = await fetch(
+		`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=${resId}`,
+	);
+	const result = await response.json();
 
-    const info = result?.data?.cards[2]?.card?.card?.info;
-    const resMenu = result?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
-    const resRecMenu = resMenu.filter((res) => res?.card?.card?.title?.includes('Recommended'))[0];
-    const items = resMenu.filter((res) => (
-        !res?.card?.card?.title?.includes('Recommended') &&
-        !res?.card?.card['@type']?.includes('NestedItemCategory') &&
-        res?.card?.card['@type']?.includes('ItemCategory')
-    ));
-    const nestedItems = resMenu.filter((res) => (res?.card?.card['@type']?.includes('NestedItemCategory')));
+	const info = result?.data?.cards[2]?.card?.card?.info;
+	const resMenu =
+		result?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+	const resRecMenu = resMenu.filter((res) =>
+		res?.card?.card?.title?.includes("Recommended"),
+	)[0];
+	const items = resMenu.filter(
+		(res) =>
+			!res?.card?.card?.title?.includes("Recommended") &&
+			!res?.card?.card["@type"]?.includes("NestedItemCategory") &&
+			res?.card?.card["@type"]?.includes("ItemCategory"),
+	);
+	const nestedItems = resMenu.filter((res) =>
+		res?.card?.card["@type"]?.includes("NestedItemCategory"),
+	);
 
-    return {
-        info,
-        recommended: {
-            title: resRecMenu?.card?.card?.title,
-            cards: resRecMenu?.card?.card?.itemCards
-        },
-        items: items?.map(item => ({
-            title: item?.card?.card?.title,
-            cards: item?.card?.card?.itemCards
-        })),
-        nestedItems: nestedItems.map((item) => ({
-            title: item?.card?.card?.title,
-            categories: item?.card?.card?.categories
-        }))
-    };
-}
+	return {
+		info,
+		recommended: {
+			title: resRecMenu?.card?.card?.title,
+			cards: resRecMenu?.card?.card?.itemCards,
+		},
+		items: items?.map((item) => ({
+			title: item?.card?.card?.title,
+			cards: item?.card?.card?.itemCards,
+		})),
+		nestedItems: nestedItems.map((item) => ({
+			title: item?.card?.card?.title,
+			categories: item?.card?.card?.categories,
+		})),
+	};
+};
--- src/components/MenuCard/index.js
+++ src/components/MenuCard/index.js
@@ -1,56 +1,72 @@
-import React, { useState } from 'react';
-import './index.css';
-import Caret from '../Caret';
-import Divider from '../Divider';
+import React, { useState } from "react";
+import "./index.css";
+import Caret from "../Caret";
+import Divider from "../Divider";
 
 const MenuCard = ({ title, cards }) => {
-    const [caret, setCaret] = useState(title === 'Recommended' ? 'up' : 'down');
+	const [caret, setCaret] = useState(title === "Recommended" ? "up" : "down");
 
-    const imgSource = (imageId) => {
-        return `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${imageId}`
-    }
+	const imgSource = (imageId) => {
+		return `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${imageId}`;
+	};
 
-    const toggleCaret = () => {
-        setCaret(caret === 'up' ? 'down' : 'up');
-    }
+	const toggleCaret = () => {
+		setCaret(caret === "up" ? "down" : "up");
+	};
 
-    const showCards = () => {
-        toggleCaret();
-    }
+	const showCards = () => {
+		toggleCaret();
+	};
 
-    const cardInfo = (card, index) => {
-        return (
-            <React.Fragment key={card?.id}>
-                <div className='menu-card-details'>
-                    <div className='menu-card-info'>
-                        <div className={`item-${card?.itemAttribute?.vegClassifier.toLowerCase()}`}></div>
-                        <div className='menu-card-name'>{card?.name}</div>
-                        <div>{`₹ ${(card?.price || card?.defaultPrice) / 100}`}</div>
-                        {card?.ratings?.aggregatedRating?.rating && card?.ratings?.aggregatedRating?.ratingCountV2 &&
-                            <div className="menu-card-rating">{`★ ${card?.ratings?.aggregatedRating?.rating} (${card?.ratings?.aggregatedRating?.ratingCountV2})`}</div>
-                        }
-                        <div className="menu-card-desc">{`${card?.description ?? ''}`}</div>
-                    </div>
-                    <img src={imgSource(card?.imageId)} className='menu-card-img' alt={`${card?.imageId} image`} />
-                </div>
-                {index < cards.length - 1 && <Divider />}
-            </React.Fragment>
-        );
-    }
+	const cardInfo = (card, index) => {
+		return (
+			<React.Fragment key={card?.id}>
+				<div className='menu-card-details flex-wrap'>
+					<div className='menu-card-info w-9/12'>
+						<div
+							className={`item-${card?.itemAttribute?.vegClassifier.toLowerCase()}`}
+						></div>
+						<div className='menu-card-name'>{card?.name}</div>
+						<div>{`₹ ${(card?.price || card?.defaultPrice) / 100}`}</div>
+						{card?.ratings?.aggregatedRating?.rating &&
+							card?.ratings?.aggregatedRating?.ratingCountV2 && (
+								<div className='menu-card-rating'>{`★ ${card?.ratings?.aggregatedRating?.rating} (${card?.ratings?.aggregatedRating?.ratingCountV2})`}</div>
+							)}
+						<div className='menu-card-desc'>{`${card?.description ?? ""}`}</div>
+					</div>
+					<div className='flex'>
+						<img
+							src={imgSource(card?.imageId)}
+							className='menu-card-img'
+							alt={`${card?.imageId} image`}
+						/>
+						<div className='absolute mt-[130px] ml-[13px] px-10 py-1 rounded-md bg-white border-[1px] border-gray-300 text-green-600 font-semibold cursor-pointer hover:bg-gray-200'>
+							Add +
+						</div>
+					</div>
+				</div>
+				{index < cards.length - 1 && <Divider />}
+			</React.Fragment>
+		);
+	};
 
-    return (
-        <div className="menu-card">
-            <div className={`menu-card-title`} style={{ marginBottom: caret === 'up' && '10px' }}>
-                <div className={`${caret === 'up' ? 'caret-down' : ''}`}>{`${title} (${cards.length})`}</div>
-                <div className="category-title-accordion" onClick={showCards}>
-                    <Caret direction={caret} />
-                </div>
-            </div>
-            {caret === 'up' && (
-                cards.map((card, index) => cardInfo(card.card.info, index))
-            )}
-        </div>
-    )
-}
+	return (
+		<div className='menu-card'>
+			<div
+				className={`menu-card-title`}
+				style={{ marginBottom: caret === "up" && "10px" }}
+			>
+				<div
+					className={`${caret === "up" ? "caret-down" : ""}`}
+				>{`${title} (${cards.length})`}</div>
+				<div className='category-title-accordion' onClick={showCards}>
+					<Caret direction={caret} />
+				</div>
+			</div>
+			{caret === "up" &&
+				cards.map((card, index) => cardInfo(card.card.info, index))}
+		</div>
+	);
+};
 
 export default MenuCard;
